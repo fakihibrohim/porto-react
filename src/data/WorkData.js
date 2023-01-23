ae5dcca986441e6cf748df1e09b8fecbedaeec81
Fakih Ibrohim <fakihibrohin37@gmail.com>
2023-01-23 14:13:30 +0700

    portfolio-final

---
+++ src/data/WorkData.js
@@ -0,0 +1,44 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"PT Bina Cipta Abadi",
+        description:"Instalasi Software (Windows, Linux). Memenuhi kebutuhan perusahaan seputar sistem komputer. Memantau dan mempertahankan kinerja jaringan agar tetap optimal. Menjawab pertanyaan yang berhubungan dengan sistem komputer. Melakukan upgrade sistem komputer yang sesuai.",
+        tags:["Windows","Linux","Ms Office"],
+        
+        demo:"https://www.linkedin.com/in/fakih-ibrohim-4b5019234/",
+        github:"https://github.com/fakihibrohim"
+    },
+    {
+        id:2,
+        name:"PT Bojong Westplas",
+        description:"Pemeliharaan semua perangkat lunak & perangkat keras, Menginstall dan mengkonfigurasi perangkat komputer, jaringan, sistem, dll. Mendiagnosis dan memecahkan kesalahan perangkat lunak dan perangkat keras komputer",
+        tags:["Windows Server","MYSQL","Network"],
+        
+        demo:"https://www.linkedin.com/in/fakih-ibrohim-4b5019234/",
+        github:"https://github.com/fakihibrohim"
+        
+    },
+    {
+        id:3,
+        name:"PT Nawakara Perkasa Nusantara",
+        description:"Bertanggung jawab atas divisi layanan terkelola, Memperbaiki bugs dan menyelesaikan case by ticket dari masing-masing cabang menggunakan aplikasi milik PT. Nawakara perkasa nusantara.",
+        tags:["react","Javascript","Node.js"],
+        
+        demo:"https://www.linkedin.com/in/fakih-ibrohim-4b5019234/",
+        github:"https://github.com/fakihibrohim"
+    },
+    {
+        id:4,
+        name:"PT KEB Hana Bank Indonesia",
+        description:"Membantu pengguna memecahkan masalah dan melakukan diagnosis Memberikan bantuan jarak jauh (remot) untuk pengguna yang tidak berada di kantor, Meningkatkan masalah yang kompleks dan berprioritas tinggi ke unit yang sesuai.",
+        tags:["Java","Spring Boot","Hibernate","JPA"],
+        
+        demo:"https://www.linkedin.com/in/fakih-ibrohim-4b5019234/",
+        github:"https://github.com/fakihibrohim"
+       
+    },
+    
+  
+] 
